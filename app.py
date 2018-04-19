196c9c42f10d81fca1612c987f9ec27747cad5b4
Prokuma <prokuma@prokuma.kr>
2018-04-19 10:51:48 +0900

    エラー修正

--- app.py
+++ app.py
@@ -6,9 +6,11 @@ import urllib
 import json
 from get_data import GetData
 
+#설정 불러오기
 f = open('information.json','r')
 settings = json.loads(f.read())
 
+# API설정
 api = twitter.Api(consumer_key=settings["consumer_key"],
                   consumer_secret=settings["consumer_secret"],
                   access_token_key=settings["access_token_key"],
@@ -30,10 +32,10 @@ class Task:
     # 트윗 형태
     template = "#Prokumaの地元天気BOT\n\n今の地元の天気\n地元：{}\n気温：{}\n降水形態：{}\n風速：{}\n\n基準日付：{}\n基準時刻：{}"
 
-    time = ""
+    time = "" # 이전시간
 
-    refresh_time = 3600 # 1시간
-    weather_dictionary = {}
+    refresh_time = 3600 # 갱신시간 : 1시간
+    weather_dictionary = {} # 날씨정보 저장
 
     def __init__(self):
         pass
@@ -45,31 +47,32 @@ class Task:
         # 기상청에서 요구하는 형태의 날짜와 시간 문자열 설정
         date, time = self.timeString()
         data.setTime(date, time)
-        print(str(data.x))
-        print(str(data.y))
         # 정보수집
         weather = data.getData()
+        # 딕셔너리에 정보 저장
         for i in weather['response']['body']['items']['item']:
-            self.weather_dictionary[i['category']] = i['fcstValue']
-
-        time = str(weather['response']['body']['items']['item'][0]['fcstTime'])
-        date = str(weather['response']['body']['items']['item'][0]['fcstDate'])
+            self.weather_dictionary[i['category']] = i['obsrValue']
 
         if time != self.time:
             # 템플릿에 정보 대입
             post = self.template.format(str(settings["region"]),str(self.weather_dictionary['T1H']) + "°C",
              pty[self.weather_dictionary['PTY']], 
             str(self.weather_dictionary['WSD']) + "m/s", str(date), str(time))
+
+            # 투고
             api.PostUpdate(post)
-            self.time = str(weather['response']['body']['items']['item'][0]['fcstTime'])
+            #시간 갱신
+            self.time = time
         threading.Timer(self.refresh_time, self.weatherTask).start()
 
-        
+    # 기상청에서 요구하는 형태로 현재시각을 알려주는 함수
     def timeString(self):
         now = datetime.datetime.now()
         date = now.strftime('%Y%m%d')
         time = int(now.strftime('%H'))
-        time_string = str(time - 1) + "00"
+        time_string = str(time) + "00"
+        if len(time_string) < 4:
+            time_string = "0" + time_string
         return date, time_string
 
 def main():
--- get_data.py
+++ get_data.py
@@ -1,7 +1,9 @@
 import json
 import urllib
+import codecs
 
 class GetData:
+    reader = codecs.getreader("utf-8")
     region_top = "http://www.kma.go.kr/DFSROOT/POINT/DATA/top.json.txt"
 
     top = ""
@@ -23,8 +25,10 @@ class GetData:
         self.leaf = leaf
         self.service_key = service_key
 
+    # 지정 지역의 좌표 받아오기
     def setRegion(self):
-        u = urllib.request.urlopen(self.region_top).read()
+        # 특별시/광역시/도
+        u = urllib.request.urlopen(self.region_top).read().decode('utf-8')
         json_top = json.loads(u)
         for i in json_top:
             if str(self.top) == str(i["value"]):
@@ -32,7 +36,9 @@ class GetData:
                 break
             else:
                 self.top_code = "11"
-        u = urllib.request.urlopen("http://www.kma.go.kr/DFSROOT/POINT/DATA/mdl." + str(self.top_code) + ".json.txt").read()
+
+        # 시/군/구
+        u = urllib.request.urlopen("http://www.kma.go.kr/DFSROOT/POINT/DATA/mdl." + str(self.top_code) + ".json.txt").read().decode('utf-8')
         json_mdl = json.loads(u)
         for i in json_mdl:
             if str(self.mdl) == str(i["value"]):
@@ -40,7 +46,9 @@ class GetData:
                 break
             else:
                 self.mdl_code = "11650"
-        u = urllib.request.urlopen("http://www.kma.go.kr/DFSROOT/POINT/DATA/leaf." + str(self.mdl_code) + ".json.txt").read()
+
+        # 동/읍/리
+        u = urllib.request.urlopen("http://www.kma.go.kr/DFSROOT/POINT/DATA/leaf." + str(self.mdl_code) + ".json.txt").read().decode('utf-8')
         json_leaf = json.loads(u)
         for i in json_leaf:
             if str(self.leaf) == str(i["value"]):
@@ -51,14 +59,15 @@ class GetData:
                 self.x = "61"
                 self.y = "125"
 
+    # 현재시각 설정
     def setTime(self, date, time):
         self.date = date
         self.time = time
 
+    # 데이터 요청
     def getData(self):
-        url1 = "http://newsky2.kma.go.kr/service/SecndSrtpdFrcstInfoService2/ForecastTimeData?&ServiceKey=" + str(self.service_key) + "&base_date=" + str(self.date) + "&base_time=" + str(self.time)
-        url2 = "&nx=" + self.x + "&ny=" + self.y + "&numOfRows=20&pageNo=1&_type=json"
-        print(url1 + url2)
-        u = urllib.request.urlopen(url1 + url2).read()
+        url1 = "http://newsky2.kma.go.kr/service/SecndSrtpdFrcstInfoService2/ForecastGrib?&ServiceKey=" + str(self.service_key) + "&base_date=" + str(self.date) + "&base_time=" + str(self.time)
+        url2 = "&nx=" + self.x + "&ny=" + self.y + "&numOfRows=40&pageNo=1&_type=json"
+        u = urllib.request.urlopen(url1 + url2).read().decode('utf-8')
         json_data = json.loads(u)
         return json_data
